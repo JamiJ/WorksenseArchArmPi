3be6cec9cb3cc428ed54712282cc7895620f60d9
JamiJ <35563202+JamiJ@users.noreply.github.com>
2021-01-26 18:18:28 +0200

    remove duplicate files

--- chromium_restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export XAUTHORITY=/home/alarm/.Xauthority
-export DISPLAY=:0
-DISPLAY=:0 xdotool key ctrl+F5
-./nettest.sh
--- nettest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 6000
-if ping -q -c 5 -W 5 8.8.8.8 >/dev/null; then
-  ./chromium_restart.sh
-else
-  reboot
-fi
